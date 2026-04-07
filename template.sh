16fbe539713d502d989c93293924c6d496b66b69
Sandeep Tomar <chaudharysandeep7060@gmail.com>
2026-04-07 13:12:27 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,17 @@
+# Creating directories for the project
+mkdir -p src 
+mdkir -p research
+
+# Creating Files for the project
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch requirements.txt
+
+
+echo "Project Directories and Files created successfully!"  
+
+
